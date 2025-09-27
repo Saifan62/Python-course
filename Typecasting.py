193074016635bfa4d8adab1cd6a7d1ebaf12d051
Saifan62 <saifanzaman681@gmail.com>
2025-09-27 16:11:14 +0600

    Add Typecasting example with variable initialization and type checks

---
+++ Typecasting.py
@@ -0,0 +1,34 @@
+name = "SaifanZaman"
+age = 12
+is_student = True
+height = 4.9
+weight = 42.4
+print("Name: " + name)
+print("data type of name: ", type(name))
+
+
+print("data type of age: ", type(age))
+
+print("Age: ", (age))
+print("data type of is_student: ", type(is_student))
+
+print("is_student: ", (is_student))
+print("Height: ", (height))
+print("data type of height: ", type(height))
+
+print("Weight: ", (weight))
+print("data type of weight: ", type(weight))
+
+print("After Typecasting")
+age = str(age)
+print(age)
+print("data type of age: ", type(age))
+height = int(height)
+print(height)
+print("data type of height: ", type(height))
+weight = int(weight)
+print(weight)
+print("data type of weight: ", type(weight))
+is_student = bool(is_student)
+print(is_student)
+print("data type of is_student: ", type(is_student))
