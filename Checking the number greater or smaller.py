431e65188842f91ec37515ac2acb04ae2413d35a
Saifan62 <saifanzaman681@gmail.com>
2025-10-04 16:03:37 +0600

    Add initial implementation of number comparison script

---
+++ Checking the number greater or smaller.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if (i<15):
+    print("I am smaller than 15")
+    print("I am an if block")
+else:
+    print("I am greater than 15")
+    print("I am an else block")
+print("I am outside if and not in else block")
