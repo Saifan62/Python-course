526cb3661762fb9277da8d742af4715c68107bc4
Saifan62 <saifanzaman681@gmail.com>
2025-10-04 15:45:20 +0600

    Add examples of string manipulation including reversal, concatenation, and case conversion

---
+++ String operation.py
@@ -0,0 +1,17 @@
+text = str(input("Enter a string: "))
+revtext = text[::-1]
+print("Reversed string is: ",)
+print(revtext)
+
+str1= "Hello, "
+str2= "How are you?"
+
+print("Concatenated string: ", str1 + str2)
+
+str = "Hello"
+print(str[::3])
+print(str[0::1])
+print(str[::-1])
+
+messege="python is fun"
+print(messege.upper())
