ed99f2939d1803be2ece2d933848a1a9499c3f26
Saifan62 <saifanzaman681@gmail.com>
2025-10-10 22:48:11 +0600

    Add script to demonstrate logical operations and string comparisons

---
+++ logical op.py
@@ -0,0 +1,18 @@
+a = 10
+b =12
+c=12
+
+print(a != b)
+print(b != c)
+
+a= "python"
+b= "java"
+
+if a != b:
+    print(a,"and",b,"are different strings")
+
+a=4
+b=5
+
+if (a == 1) != (b == 5):
+    print("One of the conditions is True")
