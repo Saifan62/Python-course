14912a93ec86782d51ecc20360911e556a722387
Saifan62 <saifanzaman681@gmail.com>
2025-10-12 10:44:07 +0600

    Add script to calculate ASCII values and identify character types

---
+++ ASCII Value.py
@@ -0,0 +1,16 @@
+ch= input("Enter a character:")
+
+if len(ch) == 1:
+    a=ord(ch)
+    print("The ASCII value of", ch, "is", a)
+    print("Hexadecimal value of", ch, "is", hex(a), "and binary value is", bin(a))
+    print("Octal value of", ch, "is", oct(a))
+if a >= 65 and a <= 90:
+    print(ch, "is an uppercase letter")
+elif a >= 97 and a <= 122:
+    print(ch, "is a lowercase letter")
+elif a >= 48 and a <= 57:
+    print(ch, "is a digit")
+
+else:
+    print("Please enter a single character")
