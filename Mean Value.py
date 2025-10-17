50a74deba92fa7f3cec44bf5d50dd11df7fccf05
Saifan62 <saifanzaman681@gmail.com>
2025-10-17 10:10:36 +0600

    Add initial implementation of mean value calculation script

---
+++ Mean Value.py
@@ -0,0 +1,10 @@
+mean1=38
+wrong_number=36
+correct_num=56
+total_numbers=40
+sum = mean1*total_numbers
+print("the sum of 40 numbers is:",sum)
+num2=sum-((wrong_number)-(correct_num))
+print("sum-((wrong_number)-(correct_num)) is:",num2)
+mean2=num2/total_numbers
+print(mean2)
