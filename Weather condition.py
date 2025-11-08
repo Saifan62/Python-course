ce966e9ca654d98bbda2f2a227bf6f4a91b1d08e
Saifan62 <saifanzaman681@gmail.com>
2025-11-08 15:57:26 +0600

    Add weather condition script to display seasonal weather information

---
+++ Weather condition.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print("The weather is pleasent in spring:", spring)
+    print("The weather is same in autumn as in spring:", autumn)
+
+spring = "autumn"
+autumn = spring
+weather_condition()
