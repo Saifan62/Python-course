db746d529c1e9953013e7c60a752470e49b89782
Saifan62 <saifanzaman681@gmail.com>
2025-11-22 16:30:14 +0600

    Fix syntax error in Math.py and add math functions for floor, ceiling, copysign, and fabs

---
+++ Math.py
@@ -0,0 +1,7 @@
+import math
+print("The floor and the ceiling of 23.56 are:"+ str(math.ceil(23.56)) + "," + str(math.floor(23.56)))
+
+x=10
+y= -15
+print(str(math.copysign(x,y))
+print(str(math.fabs(-96)))
