38316a630687355f828b521ba463bd1189f8eb28
Saifan62 <saifanzaman681@gmail.com>
2025-11-28 09:43:56 +0600

    Add initial implementation of date and time module with current date and time display

---
+++ Date,time module.py
@@ -0,0 +1,11 @@
+from datetime import date,time ,datetime
+
+
+today=date.today()
+now = datetime.now()
+print("Today's date:", today)
+print("\nCurrent time:", now)
+print("\n Current date and time:", now)
+
+
+print("\nDate components:",today.year, today.month, today.day)
