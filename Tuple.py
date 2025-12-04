604c96df8a61bbe19cd8ff0e8fc6d0e3c94bdbec
Saifan62 <saifanzaman681@gmail.com>
2025-12-04 16:39:55 +0600

    Add initial implementation of the All-in-One Geometry Dash game with UI and gameplay features

---
+++ Tuple.py
@@ -0,0 +1,24 @@
+tuplex= ("tuple",False,1.2,3,)
+print(tuplex)
+
+tuplex = (4,6,7,8,9)
+print(tuplex)
+
+#tuple are immutable,so you cannot change the values of tuple
+
+tuplex= (4,6,7,8,9)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50, 10, 60, 70 , 50)
+print(tuple1.count(50))
+
+
+tuplex= (2, 4, 6, 8, 10, 7 ,8 ,9 ,0)
+slice = tuplex[3:5]
+print(slice)
+
+_slice = tuplex[:6]
+print(_slice)
