25d26c874b2fefb2203613ca5e5ac64d7f374306
Saifan62 <saifanzaman681@gmail.com>
2025-12-05 10:45:24 +0600

    Add implementation for tuple multiplication to calculate product of elements

---
+++ Tuple multiplication.py
@@ -0,0 +1,7 @@
+tuple= (4,4,5,3,6,7)
+p=1
+
+for i in range(6):
+    p= p* tuple[i]
+
+print("The anwser is:", p)
--- Weather predic.py
+++ Weather predic.py
@@ -1,7 +1,8 @@
 weather= (1,0,0,0,1,1,0)
 sunny=0
 rainy=1
-for i in range(0,7):
+for i in range(0,7
+               ):
     if (weather[i]==0):
         rainy+=1
     else:
