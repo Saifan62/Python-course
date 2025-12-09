1e1576fcb4de76a68136c658be7931de204450e9
Saifan62 <saifanzaman681@gmail.com>
2025-12-09 13:57:36 +0600

    Add implementation to check if a number is a natural number

---
+++ Natural num.py
@@ -0,0 +1,11 @@
+number= int(input("Enter a natural number: "))
+print("Number to be checked:", number)
+
+if number > 0:
+    print(number, "is a natural number.")
+
+elif number<0:
+    print(number, "is not a natural number.")
+
+else:
+    print(number, "is neither a natural number nor a negative number.")
