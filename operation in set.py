53caf2c295109755d03e526dad4adc9abad5a9db
Saifan62 <saifanzaman681@gmail.com>
2025-12-16 19:11:14 +0600

    Add initial implementation of set operations with various examples

---
+++ operation in set.py
@@ -0,0 +1,19 @@
+my_set = {1,2,3}
+print(my_set)
+
+my_set = {1.0,"hello", (1,2,3)}
+print(my_set)
+
+my_set = {1,2,3,4,3,2}
+print(my_set)
+
+my_set = set([1,2,3,4,5])
+print(my_set,"\n")
+
+num_set = set([0,1,3,4,5])
+print("Original set:")
+print(num_set)
+
+num_set.pop()
+print("After removing an element using pop():")
+print(num_set,"\n")
