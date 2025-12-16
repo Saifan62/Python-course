c15f0095eceab782d311f03ab51fe4ea3b5847c3
Saifan62 <saifanzaman681@gmail.com>
2025-12-16 13:12:49 +0600

    Add set operations demonstration with union, intersection, and symmetric difference

---
+++ Set.py
@@ -0,0 +1,11 @@
+s1= {'blue', 'red', 'green'}
+print(s1)
+s2= {'yellow', 'red', 'pink'}
+print(s2)
+
+s3 = s1.union(s2)
+print(s3)
+s4= s1.intersection(s2)
+print(s4)
+s5= s1.symmetric_difference(s2)
+print(s5)
