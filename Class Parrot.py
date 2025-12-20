fda4096bf34a975a16d9f5223b583d6e2c8d9d12
Saifan62 <saifanzaman681@gmail.com>
2025-12-20 16:23:45 +0600

    Add initial implementation of Parrot class with attributes and instance creation

---
+++ Class Parrot.py
@@ -0,0 +1,18 @@
+class Parrot:
+
+    species = "bird"
+
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+blu= Parrot("Blu",10)
+woo= Parrot("Woo",15)
+
+print("Parrot is a ", Parrot.species)
+print("Blu is a ", blu.species)
+print("Woo is a ", woo.species)
+
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} years old".format(woo.name, woo.age))
