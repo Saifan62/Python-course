fe2e4402b69cfc610020da6193fdc1c2ce224fad
Saifan62 <saifanzaman681@gmail.com>
2026-01-15 22:39:49 +0600

    dccdc

---
+++ Pygame/Adding mario as a third sprite.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Adding images and backgrounds")
+
+background_image = pygame.transform.scale(
+    pygame.image.load('').convert(),
+    (SCREEN_WIDTH, SCREEN_HEIGHT)
+)
+
+penguin_image = pygame.transform.scale(
+    pygame.image.load('').convert_alpha(),
+    (200, 200)
+)
+penguin_rect = penguin_image.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2-30))
+
+text = pygame.font.Font(None, 36).render("Mrio", True, pygame.Color('black'))
+
+text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT + 110))
+
+def game_loop():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            display_surface.blit(background_image, (0, 0))
+        display_surface.blit(penguin_image, penguin_rect)
+        display_surface.blit(text, text_rect)
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    game_loop()
