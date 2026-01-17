24a140fa298f60e2d0f880b8bca1d5dd6958a008
Saifan62 <saifanzaman681@gmail.com>
2026-01-17 16:30:50 +0600

    Add Mario and Luigi sprites to the Pygame project

--- Pygame/Adding mario as a third sprite.py
+++ Pygame/Adding mario as a third sprite.py
@@ -4,20 +4,27 @@ pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
 
 display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Adding images and backgrounds")
+pygame.display.set_caption("Adding Mario and Luigi as sprites")
 
 background_image = pygame.transform.scale(
     pygame.image.load('').convert(),
     (SCREEN_WIDTH, SCREEN_HEIGHT)
 )
 
-penguin_image = pygame.transform.scale(
+mario_image = pygame.transform.scale(
     pygame.image.load('').convert_alpha(),
     (200, 200)
 )
-penguin_rect = penguin_image.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2-30))
+mario_rect = mario_image.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2-30))
 
-text = pygame.font.Font(None, 36).render("Mrio", True, pygame.Color('black'))
+
+luigi_image = pygame.transform.scale(
+    pygame.image.load('').convert_alpha(),
+    (200, 200)
+)
+luigi_rect = luigi_image.get_rect(center=(SCREEN_WIDTH // 2 + 100, SCREEN_HEIGHT // 2-30))
+
+text = pygame.font.Font(None, 36).render("Mario And Luigi", True, pygame.Color('red'))
 
 text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT + 110))
 
@@ -29,7 +36,8 @@ def game_loop():
                 running = False
 
             display_surface.blit(background_image, (0, 0))
-        display_surface.blit(penguin_image, penguin_rect)
+        display_surface.blit(mario_image, mario_rect)
+        display_surface.blit(luigi_image, luigi_rect)
         display_surface.blit(text, text_rect)
         pygame.display.flip()
 
