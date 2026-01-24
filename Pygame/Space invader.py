f6d858463b44074c4cd5c495d6a8c29fe5313828
Saifan62 <saifanzaman681@gmail.com>
2026-01-24 23:18:14 +0600

    Refactor code structure for improved readability and maintainability

--- Pygame/Space invader.py
+++ Pygame/Space invader.py
@@ -15,15 +15,15 @@ collision_distance = 27
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-bg = pygame.image.load('')
+bg = pygame.image.load('Space.jpg')
 
 
 pygame.display.set_caption("Space Invader")
-icon = pygame.image.load('')
+icon = pygame.image.load('UFO.png')
 pygame.display.set_icon(icon)
 
 
-player_img = pygame.image.load('')
+player_img = pygame.image.load('player.png')
 player_x = player_start_x
 player_y = player_start_y
 player_x_change = 0
@@ -33,16 +33,16 @@ enemy_x = []
 enemy_y = []
 enemy_x_change = []
 enemy_y_change = []
-num_of_enemies = 6
+num_of_enemies = 15
 
 for i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load(''))
+    enemyImg.append(pygame.image.load('enemy.webp'))
     enemy_x.append(random.randint(0, screen_width - 64))
     enemy_y.append(random.randint(enemy_start_y_min, enemy_start_y_max))
     enemy_x_change.append(enemy_speed_x)
     enemy_y_change.append(enemy_speed_y)
 
-bullet_img = pygame.image.load('')
+bullet_img = pygame.image.load('bullet.png')
 bullet_x = 0
 bullet_y = player_start_y
 bullet_x_change = 0
